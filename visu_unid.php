de98bbd80cd88bee241b3b2840eb2ecb43df5941
Gabriel Marinho <gmarinho14@gmail.com>
2022-01-11 17:34:46 -0300

    deletar unid

--- visu_unid.php
+++ visu_unid.php
@@ -18,6 +18,7 @@
         }
         
         class create_item {
+            public $id;
             public $cep;
             public $email;
             public $numero;
@@ -25,23 +26,24 @@
             public $string;
             public $html;
 
-            function __construct($cep, $email, $numero, $telefone){
-                $this->string = "<tr><td>".$cep."</td><td>".$email."</td><td>".$numero."</td><td>".$telefone."</td></tr>";
+            function __construct($id, $cep, $email, $numero, $telefone){
+                $this->string = "<tr><td>".$cep."</td><td>".$email."</td><td>".$numero."</td><td>".$telefone."</td><td><a href='alterarF.php?id=$id'>Alterar</a></td><td><a href='controle.php?id=$id&funcao=deletar&tabela=unidade&pagina=visu_unid.php'>Deletar</a></td></tr>";
                 $this->html = $this->string;
             }
         }
         
 
-        $query = "SELECT cep, numero, telefone, email FROM unidade;";
+        $query = "SELECT id, cep, numero, telefone, email FROM unidade;";
 
         $result = mysqli_query($conn, $query);
-
+        $ids = [];
         $ceps = [];
         $numeros = [];
         $telefones = [];
         $emails = [];
 
         while ($row = mysqli_fetch_assoc($result)) {
+            $ids[] = $row['id'];
             $ceps[] = strval($row['cep']);
             $emails[] =  $row['email'];
             $telefones[] = strval($row['telefone']);
@@ -54,6 +56,8 @@
         $array = [];
         
         for($i=0; $i < count($ceps); $i++){
+            $id = $ids[$i];
+
             $cep = $ceps[$i];
             
             $email = $emails[$i];
@@ -62,7 +66,7 @@
 
             $telefone = $telefones[$i];
             
-            $linha = new create_item($cep,$email,$numero,$telefone);
+            $linha = new create_item($id,$cep,$email,$numero,$telefone);
             
             $array[] = $linha;
         }
